9a9f70319b966ad1f5414a1037e3f46c195c0272
AmineOuerfellii <mohamedamine.ouerfelli@outlook.com>
2026-07-22 15:08:34 +0100

    enforce CPU/GPU distinction

--- api/activetigger/__main__.py
+++ api/activetigger/__main__.py
@@ -1,5 +1,5 @@
 import argparse
-
+from pathlib import Path
 import uvicorn
 
 from activetigger.config import config
@@ -19,12 +19,13 @@ if __name__ == "__main__":
         help="Port number for the API. Default is 5000.",
     )
     args = parser.parse_args()
-
+    reload = config.mode == "dev"
     uvicorn.run(
         "activetigger.app.main:app",
         host="0.0.0.0",
         port=args.portapi,
-        reload=config.mode == "dev",
+        reload=reload,
         reload_excludes=["*/projects/*", "*/checkpoint-*"],
         log_level="info",
+        reload_dirs=[str(Path(__file__).parent)] if reload else None,
     )
